59bd3870ac789ad5901bcd7f0c90ed1a2e4548a9
rachele <rachele.terzoli@gmail.com>
2020-03-19 15:02:50 +0100

    fine bonus 1

--- bonus1/main.js
+++ bonus1/main.js
@@ -1,4 +1,4 @@
-// var ora = ora.getHours();
-//   document.getElementById('password').innerHTML = + ora.getHours;
+
   today = new Date()
     document.write("Adesso sono le ",today.getHours(),":",today.getMinutes(),":",today.getSeconds());
+    document.write(", mancano 0 ore",((59)-today.getMinutes())," minuti ",(60-today.getSeconds())," secondi alla fine dell'ora.");
