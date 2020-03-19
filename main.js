3881a4b8389d1ec44151a63ba7a3a4c553f2231a
rachele <rachele.terzoli@gmail.com>
2020-03-19 13:20:19 +0100

    init repo

---
+++ main.js
@@ -0,0 +1,37 @@
+// finestra introduttiva
+alert("Buongiorno e benvenuti in TreniTreni, le farò qualche domanda per calcolare il prezzo del suo biglietto. In base al suo chilometraggio e  alla sua età avrà il suo prezzo personalizzato. La ringrazio per averci scelti. Prema ok per continuare ");
+// creare variabile nome e cognome
+
+// creare variabile km
+
+// moltiplicare il numero di km x 0.21 euro
+
+// creare variabile età
+
+// controllare eta se maggiore di 65 o minore di 18
+
+// se minore di 18 applicare uno sconto del 20
+
+// se maggiore di 65 applicare uno sconto del 40
+
+
+
+
+// // creare variabile nome
+// var nome;
+// // creare variabile cognome
+// var cognome;
+// // creare variabile colore preferito
+// var colorepreferito;
+//
+// // chiedere nome
+// nome = prompt("Inserisci il tuo nome idiota ");
+//
+// // chiedere cognome
+// cognome = prompt("Inserisci il tuo cognome");
+//
+// // chiedere colore preferito
+// colorepreferito = prompt("Inserisci il tuo colore preferito");
+//
+// document.getElementById('password').innerHTML = 'La tua password è : '+ nome + cognome + colorepreferito + '19';
+// // scrivi a video nomecognomecolorepreferito19 come psw
